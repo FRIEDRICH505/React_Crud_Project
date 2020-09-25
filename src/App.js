fe4aaaf0a54678505cfe395fa869db57cb9ab20d
FRIEDRICH505 <friedrich.brandt002@gmail.com>
2020-09-25 16:46:08 +0200

    React App Using Crud

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import User from './User';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import UserForm from './UserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    console.log(firebase);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/edit/:id" component={UserForm} />
+              <Route path="/add" component={UserForm} />
+              <Route exact path="/" component={User} />
+              <Route path="/*" component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+
+      </div>
+    );
+  }
 }
-
 export default App;
+
+class NotFound extends Component {
+  render() {
+    return <div>Not Found</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
 import * as serviceWorker from './serviceWorker';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBS-LksMzG8zSCHjfzzcqmDwgRHdmyHlt4",
+  authDomain: "reactcrudfirebase-93015.firebaseapp.com",
+  databaseURL: "https://reactcrudfirebase-93015.firebaseio.com",
+  projectId: "reactcrudfirebase-93015",
+  storageBucket: "reactcrudfirebase-93015.appspot.com",
+  messagingSenderId: "1010182729983",
+  appId: "1:1010182729983:web:b831820fec077ad379dcca",
+  measurementId: "G-E8EX9ZK7B3"
+};
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
